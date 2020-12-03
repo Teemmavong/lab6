a301649ef755b31fec6af7a582313833743d52ce
Teemmavong <Teemmavong@gmail.com>
2020-12-03 12:28:50 -0800

    initial commit

---
+++ lab_6.py
@@ -0,0 +1,13 @@
+"""
+We will generate a random interger between 1-20.
+Based on results of random number, we check to see if it falls under certain range.
+If num > 15 = "Cherries"
+If num > 10 = "Orange"
+If num > 5 = "Plum"
+If num > 2 = "Melon"
+If num > 1 = "Bell"
+If num not above any = "Bar"
+
+We iterate over using a loop 3 times and print results to the user. 
+For example "Plum Charries"
+"""
